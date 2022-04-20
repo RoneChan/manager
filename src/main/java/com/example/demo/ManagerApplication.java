0fe49073aef9fd9a1ffda620147b0139e96fd838
kyo <604079771@qq.com>
2022-04-20 17:54:14 +0800

    add TestRule by mucheng

--- src/main/java/com/example/demo/ManagerApplication.java
+++ src/main/java/com/example/demo/ManagerApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//定义mapper区
+@MapperScan("com.example.demo.mapper")
 public class ManagerApplication {
 
 
