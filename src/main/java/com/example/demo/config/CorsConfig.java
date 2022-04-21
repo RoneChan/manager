86aafec160d39f4dd272c041faa5afc795c32fbb
RoneChan <41538794+RoneChan@users.noreply.github.com>
2022-04-21 09:42:45 +0800

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 //        设置允许跨域的路径
         registry.addMapping("/**")
-                .allowedOrigins("*")
+ //               .allowedOrigins("*")
 //                设置允许跨域请求的域名
                 .allowedOriginPatterns("*")
 //                是否允许证书
